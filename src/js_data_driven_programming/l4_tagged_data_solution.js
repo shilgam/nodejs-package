330ab6bd549d2c31aa9aa2f5dcc96f824415eba9
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-02 04:27:50 -0700

    Previous (l3) implementation

--- src/js_data_driven_programming/l4_tagged_data_solution.js
+++ src/js_data_driven_programming/l4_tagged_data_solution.js
@@ -1,8 +1,9 @@
-import { cons, car, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
-import { cons as consList, l, random, head, reverse, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
-import { getName as getSimpleCardName, damage as simpleCardDamage } from './l4_simpleCard'; // eslint-disable-line
-import { getName as getPercentCardName, damage as percentCardDamage } from './l4_percentCard'; // eslint-disable-line
-import { typeTag } from './l4_type'; // eslint-disable-line
+import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
+import { cons as consList, l, random, head, reverse, toString as listToString,
+length, get } from 'hexlet-pairs-data'; // eslint-disable-line
+// import { getName as getSimpleCardName, damage as simpleCardDamage } from './l4_simpleCard'; // eslint-disable-line
+// import { getName as getPercentCardName, damage as percentCardDamage } from './l4_percentCard'; // eslint-disable-line
+// import { typeTag } from './l4_type'; // eslint-disable-line
 
 /*  >>>>>  EX  <<<<<
         1) simpleCard.js
@@ -24,6 +25,29 @@ const run = (player1, player2, cards, customRandom) => {
   const iter = (health1, name1, health2, name2, order, log) => {
     // BEGIN (write your solution here)
 
+    if (health1 <= 0) {
+      return consList(cons(car(head(log)), `${name1} был убит`), log);
+    }
+    const card = customRandom(cards);
+
+    const cardName = car(card);
+    console.log(`>>>>> cardName: ${cardName}`);
+    const damage = cdr(card)(health2);
+    const newHealth = health2 - damage;
+    const message = [
+      `Игрок '${name1}' применил '${cardName}' против `,
+      `'${name2}' и нанес урон '${damage}'`,
+    ].join('');
+
+    let stats;
+    if (order === 1) {
+      stats = cons(cons(health1, newHealth), message);
+    } else if (order === 2) {
+      stats = cons(cons(newHealth, health1), message);
+    }
+    const newLog = consList(stats, log);
+    return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, newLog);
+
     // END
   };
 
@@ -32,6 +56,31 @@ const run = (player1, player2, cards, customRandom) => {
   return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
 };
 
-export default (cards, customRandom = random) =>
-  (name1, name2) =>
-    run(name1, name2, cards, customRandom);
+const make = (cards, customRandom) =>
+  (player1, player2) => run(player1, player2, cards, customRandom);
+
+export default make;
+
+
+const cards = l(
+  cons('Костяная кочерга гробницы', () => 7),
+  cons('Памятный металл палача', health => Math.round(health * 0.8)),
+);
+
+let cardIndex = 2;
+const pseudoRandomFunc = (cardsPack) => {
+  cardIndex = cardIndex === 0 ? 1 : 0;
+  return get(cardIndex, cardsPack);
+};
+
+// // Rinning game with random function OR for testing:
+const game = make(cards, pseudoRandomFunc); // random
+
+const gameLog = game('John', 'Ada');
+
+console.log(`>>>>   Number of steps:  ${length(gameLog)}`);
+console.log(`>>>>   get0: ${pairToString(get(0, gameLog))}`); // .toBe('(10, 10)')
+console.log(`>>>>   get1: ${pairToString(get(1, gameLog))}`); // .toBe('(10, 3)')
+console.log(`>>>>   get2: ${pairToString(get(2, gameLog))}`); // .toBe('(2, 3)')
+console.log(`>>>>   get3: ${pairToString(get(3, gameLog))}`); // .toBe('(2, -4)')
+console.log(`>>>>   get4: ${pairToString(get(4, gameLog))}`); // .toBe('(2, -4)')
