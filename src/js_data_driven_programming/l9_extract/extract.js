a141d4119c1cf97587de27e6620998a6f1fd2ee5
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-06 03:28:17 -0700

    Implementation

--- src/js_data_driven_programming/l9_extract/extract.js
+++ src/js_data_driven_programming/l9_extract/extract.js
@@ -1,5 +1,5 @@
-import { toString, map } from 'hexlet-pairs-data'; // eslint-disable-line
-import { getAttribute, getName } from './tags';
+import { toString as listToString, l, map } from 'hexlet-pairs-data'; // eslint-disable-line
+import { make, getAttribute, getName } from './tags';
 
 /*  >>>>>  EX  <<<<<
   В HTML, некоторые элементы хранят ссылку в атрибуте href, а некоторые в src. Например:
@@ -38,4 +38,24 @@ import { getAttribute, getName } from './tags';
 
 // BEGIN (write your solution here)
 
-// END
+const getAttrName = (tag) => {
+  const attributes = tag.attributes;
+  return Object.keys(attributes)[0];
+};
+
+const extract = (tags) => {
+  const getAttrBody = (tag) => {
+    const attrName = getAttrName(tag);
+    return getAttribute(attrName, tag);
+  };
+  return map(getAttrBody, tags);
+};
+export default extract;
+
+// // Testing
+// const tags = l(
+//   make('a', { href: '/about' }),
+//   make('img', { src: '/avatar.jpeg' }),
+//   make('link', { href: '/favicon.ico' }),
+// );
+// console.log(`>>>>> ${listToString(extract(tags))}`);
