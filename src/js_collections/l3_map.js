21da62766081e852071634c809fad2f673195076
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-09 02:45:02 -0700

    Implementation

--- src/js_collections/l3_map.js
+++ src/js_collections/l3_map.js
@@ -21,4 +21,44 @@ stopWords это список стоп-слов, то есть эти слова
 
 // BEGIN (write your solution here)
 
+const occurrences = (elem, arr) => arr.filter(currEl => currEl === elem).length;
+
+const removeOccurences = (word, arr) => arr.filter(elem => (elem !== word));
+
+const removeStopWords = (words, stopWords) => {
+  let iterList = words;
+  stopWords.forEach((elem) => {
+    iterList = removeOccurences(elem, iterList);
+  });
+  return iterList;
+};
+
+export const wordsCount = (words, stopWords) => {
+  const wordsInLowerCase = words.map(word => word.toLowerCase());
+  // console.log(`>>>>    LowerCase:  ${wordsInLowerCase}`);
+  const pureWords = removeStopWords(wordsInLowerCase, stopWords);
+  // console.log(`>>>>    pureWords:  ${pureWords}`);
+
+  const map = new Map();
+
+  const iter = (wordsList) => {
+    if (wordsList.length === 0) {
+      return map;
+    }
+    const word = wordsList[0];
+    const counter = occurrences(word, wordsList);
+    map.set(word, counter);
+    const newList = removeOccurences(word, wordsList);
+    return iter(newList);
+  };
+  return iter(pureWords);
+};
+
+export default wordsCount;
 // END
+
+// // OUTPUT:
+// const stopWords = ['and', 'or', 'a', 'the', ''];
+// const words = ['HellO', 'h', 'And', 'heLlo', '', 'AND', 'DOG', 'oR', 'cat', 'HELLO', 'caT'];
+// const myMap = wordsCount(words, stopWords);
+// myMap.forEach((key, value) => console.log(`>> key: ${key},   val: ${value}`));
