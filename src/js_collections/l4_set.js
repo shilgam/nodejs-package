7d09de005f0305a640deccaafce1cf30adf5e408
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-10 11:23:12 -0700

    Solution

--- src/js_collections/l4_set.js
+++ src/js_collections/l4_set.js
@@ -5,3 +5,12 @@
 
 > difference(new Set([2, 1]), new Set([2, 3]));   // → [1]
 */
+
+const difference = (setA, setB) =>
+  new Set([...setA].filter(elem => !setB.has(elem)));
+
+export default difference;
+
+// // Testing
+// const diffSet = difference(new Set([2, 1]), new Set([2, 3])); // new Set([1])
+// diffSet.forEach((key, value) => console.log(`>> key: ${key},   val: ${value}`));
