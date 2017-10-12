264bf28a8fd5af78afbbdab5cc3a67a341ea13fc
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-12 07:53:50 +0300

    Previous (l5) mutable implementation

--- src/js_collections/l6_immutable.js
+++ src/js_collections/l6_immutable.js
@@ -9,13 +9,29 @@ class Enumerable {
 
   select(fn) {
     // BEGIN (write your solution here)
-
+    this.collection = this.collection.map(fn);
+    return this;
     // END
   }
 
   orderBy(fn, direction = 'asc') {
     // BEGIN (write your solution here)
+    const comparator = (a, b) => {
+      const elA = fn(a);
+      const elB = fn(b);
+      const compareResult = direction === 'asc' ? 1 : -1;
 
+      if (elA > elB) {
+        return compareResult;
+      } else if (elA < elB) {
+        return -compareResult;
+      }
+
+      return 0;
+    };
+
+    this.collection.sort(comparator);
+    return this;
     // END
   }
 
@@ -30,3 +46,25 @@ class Enumerable {
 }
 
 export default Enumerable;
+
+// // TESTING
+// const putsArray = arr => arr.forEach(el => console.log(el));
+//
+// const cars = [
+//   { brand: 'bmw', model: 'm5', year: 2014 },
+//   { brand: 'bmw', model: 'm4', year: 2013 },
+//   { brand: 'kia', model: 'sorento', year: 2014 },
+//   { brand: 'kia', model: 'rio', year: 2010 },
+//   { brand: 'kia', model: 'sportage', year: 2012 },
+// ];
+// // putsArray(cars);
+//
+// const coll = new Enumerable(cars);
+//
+// // // Test EX1: select
+// // const models = coll.select(car => car.model);
+// // putsArray(models.toArray()); // ['m5', 'm4', 'sorento', 'rio', 'sportage']);
+//
+// // Test EX2: OrderBy
+// const ordItems = coll.orderBy(car => car.model).select(car => car.model);
+// putsArray(ordItems.toArray()); // ["m4", "m5", "rio", "sorento", "sportage"]
