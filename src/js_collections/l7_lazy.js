d41424cfe8f1fc8b0cf8c96b492bbb5d616a2200
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-12 08:56:07 +0300

    Exercise

---
+++ src/js_collections/l7_lazy.js
@@ -0,0 +1,16 @@
+/*  >>>>>  EX  <<<<<
+Реализуйте ленивую версию Enumerable. Интерфейс включает в себя следующие методы:
+select, where, orderBy, toArray.
+
+    ПОДСКАЗКИ:
+Так как коллекция ленивая, не нужно выполнять вычислений до вызова toArray,
+вместо этого необходимо формировать коллекцию из отложенных вычислений.
+*/
+
+class Enumerable {
+  // BEGIN (write your solution here)
+
+  // END
+}
+
+export default Enumerable;
