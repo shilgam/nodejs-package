6ce6f9ee0055b10b373cbfe0e7e33d20dbf573d7
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-13 07:47:59 -0700

    Added previous (l7) solution

--- src/js_collections/l8_memorize.js
+++ src/js_collections/l8_memorize.js
@@ -41,8 +41,41 @@ class Enumerable {
   }
 
   // BEGIN (write your solution here)
-
+  toArray() {
+    return this.operations.reduce((accum, operation) => operation(accum), this.collection);
+  }
   // END
 }
 
 export default Enumerable;
+
+// /* TESTING: */
+// const putsArray = arr => arr.forEach(el => console.log(el));
+//
+// const cars = [
+//   { brand: 'bmw', model: 'm5', year: 2014 },
+//   { brand: 'bmw', model: 'm4', year: 2013 },
+//   { brand: 'kia', model: 'sorento', year: 2014 },
+//   { brand: 'kia', model: 'rio', year: 2010 },
+//   { brand: 'kia', model: 'sportage', year: 2012 },
+// ];
+//
+// const coll = new Enumerable(cars);
+//
+// /* SELECT */ console.log('> > > > EX1: SELECT');
+// const selected = coll.select(car => car.model);
+// putsArray(selected.toArray()); // ['m5', 'm4', 'sorento', 'rio', 'sportage']);
+// console.log('---------');
+//
+// /* ORDER BY */ console.log('> > > > EX2: ORDER BY');
+// const sorted = coll.orderBy(car => car.model).select(car => car.model);
+// putsArray(sorted.toArray()); // ["m4", "m5", "rio", "sorento", "sportage"]
+// console.log('---------');
+//
+// /* IMMUTABLE */ console.log('> > > > EX3: SHOULD BE IMMUTABLE');
+// const result = coll.where(car => car.brand === 'kia')
+//   .where(car => car.year > 2011).select(car => car.model);
+// putsArray(result.toArray()); // ['sorento', 'sportage']
+// console.log('- - - - -');
+// coll.collection.push({ brand: 'kia', model: 'optima', year: 2013 });
+// putsArray(result.toArray()); // ['sorento', 'sportage']
