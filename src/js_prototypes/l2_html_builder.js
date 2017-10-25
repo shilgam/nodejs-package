cf0ef7c2810b330d388a98ab8c3ee487e22aa1ed
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-25 13:56:13 +0300

    Solution

--- src/js_prototypes/l2_html_builder.js
+++ src/js_prototypes/l2_html_builder.js
@@ -11,24 +11,47 @@ import _ from 'lodash'; // eslint-disable-line
 */
 
 // BEGIN (write your solution here)
+const buildHtml = (tag) => {
 
+  const iter = (chain, accum) => {
+    const tagName = chain[0];
+    let attributes = '';
+    const tail = chain.slice().splice(1);
+    const newTail = tail.map((el) => {
+      if (typeof el === 'string') {
+        return el;
+      } else if (el instanceof Array) {
+        const children = el.map(child => iter(child, []));
+        return children.join('');
+      } else if (el instanceof Object) {
+        const keys = Object.keys(el);
+        const attr = keys.map(key => `${key}="${el[key]}"`).join(' ');
+        attributes = ` ${attr}`;
+      }
+      return null; // TO DO
+    });
+    return `<${tagName}${attributes}>${newTail.join('')}</${tagName}>`;
+  };
+  return iter(tag, []);
+};
+export default buildHtml;
 // END
 
 /* TESTING */
-const data = ['html', [
-  ['meta', [
-    ['title', 'hello, hexlet!'],
-  ]],
-  ['body', { class: 'container' }, [
-    ['h1', { class: 'header' }, 'html builder example'],
-    ['div', [
-      ['span', 'span text2'],
-      ['span', 'span text3'],
-    ]],
-  ]],
-]];
-
-buildHtml(data);
+// const data = ['html', [
+//   ['meta', [
+//     ['title', 'hello, hexlet!'],
+//   ]],
+//   ['body', { class: 'container' }, [
+//     ['h1', { class: 'header' }, 'html builder example'],
+//     ['div', [
+//       ['span', 'span text2'],
+//       ['span', 'span text3'],
+//     ]],
+//   ]],
+// ]];
+//
+// console.log(buildHtml(data));
 /*  <html>
       <meta><title>hello, hexlet!</title></meta>
       <body class="container">
