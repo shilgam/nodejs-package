193102b4472975b4765f7471f8423a713986e359
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-29 23:42:26 +0300

    Previous (l4) implementation

---
+++ src/js_prototypes/l6_inheritance/PairedTag.js
@@ -0,0 +1,18 @@
+export default class PairedTag {
+  constructor(name, attributes = {}, body = '', children = []) {
+    this.name = name;
+    this.attributes = attributes;
+    this.body = body;
+    this.children = children;
+  }
+
+  toString() {
+    const content = (this.children.length === 0) ? this.body : this.children.map(child => child.toString()).join('');
+    return `<${this.name}${this.getAttributesAsLine()}>${content}</${this.name}>`;
+  }
+
+  getAttributesAsLine() {
+    return Object.keys(this.attributes)
+      .map(key => ` ${key}="${this.attributes[key]}"`).join('');
+  }
+}
---
+++ src/js_prototypes/l6_inheritance/buildNode.js
@@ -0,0 +1,10 @@
+import PairedTag from './PairedTag';
+import SingleTag from './SingleTag';
+
+const singleTagsList = new Set(['hr', 'img', 'br']);
+
+const buildNode = (name, ...args) => {
+  const C = (singleTagsList.has(name)) ? SingleTag : PairedTag;
+  return new C(name, ...args);
+};
+export default buildNode;
