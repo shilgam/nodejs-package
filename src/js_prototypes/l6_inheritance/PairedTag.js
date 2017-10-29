c4eca837a08c924ec99e950b09b33115d350232a
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-10-30 00:21:08 +0300

    Solution

--- src/js_prototypes/l6_inheritance/PairedTag.js
+++ src/js_prototypes/l6_inheritance/PairedTag.js
@@ -1,3 +1,15 @@
 // BEGIN (write your solution here)
+import Node from './Node';
 
+export default class PairedTag extends Node {
+  constructor(name, attributes = {}, body = '', children = []) {
+    super(name, attributes, body, children);
+  }
+
+  toString() {
+    const content = (this.children.length === 0) ?
+      this.body : this.children.map(child => child.toString()).join('');
+    return `<${this.name}${this.getAttributesAsLine()}>${content}</${this.name}>`;
+  }
+}
 // END
--- src/js_prototypes/l6_inheritance/SingleTag.js
+++ src/js_prototypes/l6_inheritance/SingleTag.js
@@ -1,3 +1,13 @@
 // BEGIN (write your solution here)
+import Node from './Node';
 
+export default class SingleTag extends Node {
+  constructor(name, attributes = {}, body = '', children = []) {
+    super(name, attributes, body, children);
+  }
+
+  toString() {
+    return `<${this.name}${this.getAttributesAsLine()}>`;
+  }
+}
 // END
