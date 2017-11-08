964aff35c2d8d33a4e99eda79f63c0b4ab2348ec
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-11-08 13:48:59 +0300

    Solution

--- src/js_prototypes/l9_prototypes/PairedTag.js
+++ src/js_prototypes/l9_prototypes/PairedTag.js
@@ -1,18 +1,17 @@
 // BEGIN (write your solution here)
 import Node from './Node';
 
-function toString() {
-  const content = (this.children.length === 0) ?
-    this.body : this.children.map(child => child.toString()).join('');
-  return `<${this.name}${this.getAttributesAsLine()}>${content}</${this.name}>`;
-}
-
 export default function PairedTag(name, attributes = {}, body = '', children = []) {
   Node.apply(this, [name, attributes]);
   this.body = body;
   this.children = children;
-  this.toString = toString;
 }
+
+PairedTag.prototype.toString = function toString() {
+  const content = (this.children.length === 0) ?
+    this.body : this.children.map(child => child.toString()).join('');
+  return `<${this.name}${this.getAttributesAsLine()}>${content}</${this.name}>`;
+};
 // END
 
 /* DEBUG */
--- src/js_prototypes/l9_prototypes/SingleTag.js
+++ src/js_prototypes/l9_prototypes/SingleTag.js
@@ -1,14 +1,13 @@
 // BEGIN (write your solution here)
 import Node from './Node';
 
-function toString() {
-  return `<${this.name}${this.getAttributesAsLine()}>`;
-}
-
 export default function SingleTag(name, attributes = {}) {
   Node.apply(this, [name, attributes]);
-  this.toString = toString;
 }
+
+SingleTag.prototype.toString = function toString() {
+  return `<${this.name}${this.getAttributesAsLine()}>`;
+};
 // END
 
 /* DEBUG */
