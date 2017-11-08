fcb334d48af9d68ffcd219c3b8252b74ed87d43d
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-11-08 13:28:43 +0300

    Exercise

---
+++ src/js_prototypes/l9_prototypes/Node.js
@@ -0,0 +1,11 @@
+function getAttributesAsLine() {
+  return Object.keys(this.attributes).reduce((acc, key) =>
+    `${acc} ${key}="${this.attributes[key]}"`, '');
+}
+
+export default function Node(name, attributes = {}) {
+  this.name = name;
+  this.attributes = attributes;
+
+  this.getAttributesAsLine = getAttributesAsLine;
+}
