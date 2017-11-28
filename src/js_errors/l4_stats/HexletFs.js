8ba54f9ca5757aae92e22dc95e58dde615bcf684
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2017-11-28 16:33:00 +0300

    Prev (l3) implementation

--- src/js_errors/l4_stats/HexletFs.js
+++ src/js_errors/l4_stats/HexletFs.js
@@ -1,15 +1,16 @@
 import path from 'path';
 import Tree from 'hexlet-trees'; // eslint-disable-line
 
-import Dir from './Dir';
-import File from './File';
+// import Dir from './Dir';
+// import File from './File';
 
 const getPathParts = filepath =>
   filepath.split(path.sep).filter(part => part !== '');
 
-export default class {
+export default class HexletFs {
   constructor() {
-    this.tree = new Tree('/', new Dir('/'));
+    // this.tree = new Tree('/', new Dir('/'));
+    this.tree = new Tree('/', { type: 'dir' });
   }
 
   statSync(filepath) {
@@ -19,12 +20,24 @@ export default class {
 
   touchSync(filepath) {
     const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new File(base));
+    // return this.findNode(dir).addChild(base, new File(base));
+    return this.findNode(dir).addChild(base, { type: 'file' });
   }
 
   mkdirSync(filepath) {
     const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new Dir(base));
+    // return this.findNode(dir).addChild(base, new Dir(base));
+    return this.findNode(dir).addChild(base, { type: 'dir' });
+  }
+
+  isDirectory(filepath) {
+    const current = this.findNode(filepath);
+    return current && current.getMeta().type === 'dir';
+  }
+
+  isFile(filepath) {
+    const current = this.findNode(filepath);
+    return current && current.getMeta().type === 'file';
   }
 
   findNode(filepath) {
@@ -32,3 +45,16 @@ export default class {
     return parts.length === 0 ? this.tree : this.tree.getDeepChild(parts);
   }
 }
+
+/* DEBUG */
+// const files = new HexletFs();
+//
+// console.log(`isDirectory?  ${files.isDirectory('/etc')}`); // false
+// files.mkdirSync('/etc');
+// console.log(`isDirectory?  ${files.isDirectory('/etc')}`); // true
+//
+// files.mkdirSync('/etc/nginx');
+// console.log(`isDirectory?  ${files.isDirectory('/etc/nginx')}`); // true
+//
+// files.touchSync('/etc/file.txt');
+// console.log(`isFile?  ${files.isFile('/etc/file.txt')}`); // true
