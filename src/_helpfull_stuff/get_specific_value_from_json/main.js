8dae94f57cf022ddde4c3e45e4143e9712a613ea
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-02-22 12:42:51 +0300

    MIB-2235: Calculate aggregated values for _OTHER_LCI_ dim value

--- src/_helpfull_stuff/get_specific_value_from_json/main.js
+++ src/_helpfull_stuff/get_specific_value_from_json/main.js
@@ -2,11 +2,27 @@ import path from 'path';
 
 const fs = require('fs');
 
-const dataPath = 'data.json';
+const pcaMetricsContent = fs.readFileSync(path.resolve(__dirname, 'pca_metrics.json'));
 
-const content = fs.readFileSync(path.resolve(__dirname, dataPath));
+const dimValuesContent = fs.readFileSync(path.resolve(__dirname, 'dim_values.txt'));
 
-console.log('Output Content : ');
-// console.log(JSON.parse(content));
+const dimValuesArray = dimValuesContent.toString().split('\n')
+  .filter(el => el !== '') // remove empty lines
+  .filter(el => !el.includes('//')); // remove commented lines
 
-console.log(JSON.parse(content).data.map(x => x.id));
+console.log(`Number of aggregated dim values: ${dimValuesArray.length}.\n`);
+
+console.log('CALCULATION:');
+// console.log(JSON.parse(pcaMetricsContent));
+
+const acc = (accumulator, currentValue) => accumulator + currentValue;
+
+const arrOfParam = param => dimValuesArray.map(dimName =>
+  JSON.parse(pcaMetricsContent).kpi_pca_metrics.visit_metrics.dimensions[dimName][param]);
+
+arrOfParam('impressions').reduce(acc);
+
+console.log(`impressions: ${arrOfParam('impressions').reduce(acc)}`);
+console.log(`post_exposure_visits: ${arrOfParam('post_exposure_visits').reduce(acc)}`);
+console.log(`total_visits_projected: ${arrOfParam('total_visits_projected').reduce(acc)}`);
+console.log(`exposed_visits: ${arrOfParam('exposed_visits').reduce(acc)}`);
