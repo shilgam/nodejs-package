0bab642790f46034602c4034cdff9a1c9e7c2e2f
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-04-23 15:21:49 +0300

    Solution

--- src/08_automata-based_programming/l6_state_machine_pattern/Order.js
+++ src/08_automata-based_programming/l6_state_machine_pattern/Order.js
@@ -4,7 +4,7 @@ import StateMachine from 'javascript-state-machine'; // eslint-disable-line
 export default class Order {
   constructor(items) {
     this.items = items;
-    this.history = [];
+    this.history = this.items || [];
 
     this._fsm(); // eslint-disable-line
   }
@@ -14,12 +14,22 @@ StateMachine.factory(Order, {
   init: 'init',
   transitions: [
     // BEGIN (write your solution here)
-
+    { name: 'accept', from: 'init', to: 'pending' },
+    { name: 'ship', from: 'pending', to: 'shipped' },
+    { name: 'complete', from: 'shipped', to: 'completed' },
+    { name: 'cancel', from: 'init', to: 'canceled' },
+    { name: 'cancel', from: 'pending', to: 'canceled' },
+    { name: 'refund', from: 'shipped', to: 'refunded' },
+    { name: 'refund', from: 'completed', to: 'refunded' },
     // END
   ],
   methods: {
     // BEGIN (write your solution here)
-
+    onEnterState() {
+      if (!this._fsm.is('init')) {  // eslint-disable-line
+        this.history.push({ state: this._fsm.state, createdAt: new Date() }); // eslint-disable-line
+      }
+    },
     // END
   },
 });
